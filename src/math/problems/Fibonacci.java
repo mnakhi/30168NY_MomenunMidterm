e031c0d9cf33cf5d82bd0893687c4398ec3d68a4
mnakhi <mnessaakhi@gmail.com>
2020-01-12 13:08:11 -0500

    Fibonacci solution

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -6,7 +6,24 @@ public class Fibonacci {
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
+         int max = 40;
+         for(int i = 0; i<=max;i++){
+             System.out.println(findFibonacci(i));
+         }
 
+    }
+    public static int findFibonacci(int num){
+        if (num==0){
+            return 0;
+        }
+        if(num == 1){
+            return 1;
+        }
+        if (num==2){
+            return 1;
+        }
+
+        return findFibonacci(num-1)+ findFibonacci(num-2);
     }
 
 }
