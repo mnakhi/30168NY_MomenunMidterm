8ed141aac97a37555ead1988a4f7107e6d26f804
mnakhi <mnessaakhi@gmail.com>
2020-01-13 22:57:53 -0500

    data structure

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,7 +12,31 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
+		ArrayList<String>studentName = new ArrayList<>();
+		studentName.add("John");
+		studentName.add("Chris");
+		studentName.add("Jenny");
+		studentName.add("Mike");
+		studentName.add("Tom");
+		studentName.add("Sherry");
+		studentName.add("Alex");
+		studentName.add("Nancy");
+
+		studentName.remove("Tom"); //Removes Tom from the list
+		studentName.remove(2);// Removes element of index 2 which is Jenny
+		System.out.println(studentName.get(3)); // It prints out the element of index 3, here the updated index 3's element is Sherry
+
+		System.out.println("");
+		System.out.println("Using for each loop to retrieve data: ");
+		for (String s:studentName) {
+			System.out.println(s);
+		}
+		System.out.println(" ");
+		System.out.println("Using while loop with iterator to retrieve data:");
+		Iterator iterator = studentName.iterator();
+		while (iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
 
 	}
 
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +12,44 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+		Map<String, List<String>>list = new HashMap<String, List<String>>();
+		String studentName1 = "Ashley";
+		String studentName2 = "Ron";
+		String studentName3 = "Charlie";
+		String studentName4 = "Rachel";
+
+		List<String>student1Info = new LinkedList<String>();
+		student1Info.add("123 Fake Street");
+		student1Info.add("xyz@gmail.com");
+
+		List<String>student2Info = new LinkedList<String>();
+		student2Info.add("456 Fake Street");
+		student2Info.add("pqr@gmail.com");
+
+		List<String>student3Info = new LinkedList<String>();
+		student3Info.add("789 Fake Street");
+		student3Info.add("abc@gmail.com");
+
+		List<String>student4Info = new LinkedList<String>();
+		student4Info.add("135 Fake Street");
+		student4Info.add("uvw@gmail.com");
+
+		list.put(studentName1,student1Info);
+		list.put(studentName2,student2Info);
+		list.put(studentName3,student3Info);
+		list.put(studentName4,student4Info);
+
+		System.out.println("Using for each loop to retrieve data : ");
+		for(Map.Entry m:list.entrySet()){
+			System.out.println("Key = "+m.getKey()+ " , "+"Value = "+m.getValue());
+		}
+		System.out.println();
+		System.out.println("Using while loop with iterator to retrieve data : ");
+		Iterator <Map.Entry<String, List<String>>>iterator= list.entrySet().iterator();
+		while (iterator.hasNext()){
+			Map.Entry<String, List<String>>entry = iterator.next();
+			System.out.println("Key = "+entry.getKey()+" , "+"Value= "+entry.getValue());
+		}
 
 	}
 
