2e8e0242c4aa5da5057078abd24a4e3b57877c32
mnakhi <mnessaakhi@gmail.com>
2020-01-13 23:21:27 -0500

    data structure

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +12,29 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		Queue<Integer>ticketNumber = new PriorityQueue<>();
+		ticketNumber.add(122);
+		ticketNumber.add(123);
+		ticketNumber.add(311);
+		ticketNumber.add(257);
+		ticketNumber.add(178);
+		ticketNumber.add(200);
+		ticketNumber.add(120);
+
+		ticketNumber.peek(); //Retrieves, but does not remove, the head of this queue in this case 120
+
+		ticketNumber.remove();//Retrieves and removes the head of this queue, in this case retrieves and removes 120
+		ticketNumber.poll();//Retrieves and removes the head of this queue, in the case retrieves and removes the new head 122
+		System.out.println("Using for each loop to retrieve the ticketNumber queue: ");
+		for (Integer i:ticketNumber) {
+			System.out.println(i);
+		}
+		System.out.println();
+		System.out.println("Using while loop with iterator to retrieve the ticketNumber queue: ");
+		Iterator iterator = ticketNumber.iterator();
+		while (iterator.hasNext()){
+			System.out.println(iterator.next());
+		}
 
 	}
 
